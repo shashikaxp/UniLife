a7c486854d2c7ae61cc98223a21cd9c884617366
Shashika <shashikaxp@gmail.com>
2020-12-18 09:27:18 +1100

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 100f;
+    public float sideForce = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        } else if (Input.GetKey("d")) {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
