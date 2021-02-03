675f2d8d62f4e70068b3f3778c2cd7838f1ef551
Shashika <shashikaxp@gmail.com>
2021-02-03 12:41:39 +1100

    add player controllers

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
+
     public float forwardForce = 100f;
-    public float sideForce = 100f;
+
+    private CharacterController controller;
+    private Vector3 direction;
+    private int lane =  1; // 0:left, 1:center, 2:right
+    private float laneDistance = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
-        if (Input.GetKey("a")) {
-            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        } else if (Input.GetKey("d")) {
-            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        direction.z = forwardForce;
+        if (Input.GetKeyDown("d"))
+        {
+            lane++;
+            if (lane == 3) {
+                lane = 2;
+            }
         }
+        else if (Input.GetKeyDown("a"))
+        {;
+            lane--;
+            if (lane == -1)
+            {
+                lane = 0;
+            }
+        }
+
+
+        // calculate the next lane position
+        Vector3 position = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (lane == 0) {
+            position += Vector3.left * laneDistance;
+        } else if (lane == 2) {
+            position += Vector3.right * laneDistance;
+        }
+
+        transform.position =  Vector3.Lerp(transform.position, position, 80*Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime);
     }
 }
